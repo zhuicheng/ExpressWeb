84671d9faabff2c00a1b457c90cd8ca4c627682f
馬驍 <wawl89757@126.com>
2014-11-11 15:52:12 +0800

    ...

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log('Express start..');
 
+var path = require('path');
+var fs = require('fs');
+var ejs = require('ejs');
 var express = require('express');
 var app = express();
 
-app.use(function(req, res, next) {
-	console.error("当前时间：%s", Date.now());
-	next();
-});
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/zh', function(req, res) {
 	res.send('你好');
@@ -17,7 +17,24 @@ app.get('/en', function(req, res) {
 });
 
 app.get('/', function(req, res) {
-	res.redirect('/zh');
+	// res.redirect('/zh');
+	fs.readFile(path.join(__dirname, './views/index.js'), function(err, fd) {
+		if (err) {
+			console.log(err);
+			throw err;
+		}
+		
+		var users = [];
+		users.push({ name: 'Tobi', age: 2, species: 'ferret' });
+		users.push({ name: 'Loki', age: 2, species: 'ferret' });
+		users.push({ name: 'Jane', age: 6, species: 'ferret' });
+		
+		res.send(ejs.render(fd.toString(), {
+			title : 'hereee..',
+			content : '首页',
+			users: users
+		}));
+	});
 });
 
 app.listen(8888);
---
+++ views/index.js
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title><%=title %></title>
+	</head>
+	<body>
+		<h2><%=content %></h2>
+		<div>人员名单</div>
+		<% users.forEach(function(u) { %>
+			<div>
+				<span>姓名：<%=u.name %></span>
+				<span style='margin-left: 20px;'>年龄：<%=u.age %></span>
+			</div>
+		<% }) %>
+	</body>
+</html>
