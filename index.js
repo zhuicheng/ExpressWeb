c67a1288e71c2ac3072ca2aa17a721badf35c3dd
馬驍 <wawl89757@126.com>
2014-11-11 14:22:39 +0800

    ...

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('Express start..');
+
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+	console.error("当前时间：%s", Date.now());
+	next();
+});
+
+app.get('/zh', function(req, res) {
+	res.send('你好');
+});
+
+app.get('/en', function(req, res) {
+	res.send('hello world');
+});
+
+app.get('/', function(req, res) {
+	res.redirect('/zh');
+});
+
+app.listen(8888);
