7d21da4c29a33adec6200fccdabfc2353d64bf5f
馬驍 <wawl89757@126.com>
2014-11-17 09:38:47 +0800

    ...

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ console.log('Express start..');
 
 var path = require('path');
 var fs = require('fs');
+var mysql = require('mysql');
 var ejs = require('ejs');
 var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/data', function(req, res) {
+	var conn = mysql.createConnection({
+		host : '192.168.20.239',
+		user : 'root',
+		password : 'root',
+		database : 'uccp'
+	});
+
+	conn.query('SELECT * FROM T_CM_USER', function(err, rows, fields) {
+		if (err) {
+			console.error('%s', err);
+			throw err;
+		}
+
+		fs.readFile(path.join(__dirname, './views/index.html'), function(err, fd) {
+			if (err) {
+				console.log(err);
+				throw err;
+			}
+
+			res.send(ejs.render(fd.toString(), {
+				title : 'hereee..',
+				content : '首页',
+				tableName: 'T_CM_USER',
+				users : rows
+			}));
+		});
+	});
+});
 
 app.get('/zh', function(req, res) {
 	res.send('你好');
@@ -23,16 +54,25 @@ app.get('/', function(req, res) {
 			console.log(err);
 			throw err;
 		}
-		
+
 		var users = [];
-		users.push({ name: 'Tobi', age: 2, species: 'ferret' });
-		users.push({ name: 'Loki', age: 2, species: 'ferret' });
-		users.push({ name: 'Jane', age: 6, species: 'ferret' });
-		
+		users.push({
+			name : 'Tobi',
+			age : 2
+		});
+		users.push({
+			name : 'Loki',
+			age : 2
+		});
+		users.push({
+			name : 'Jane',
+			age : 6
+		});
+
 		res.send(ejs.render(fd.toString(), {
 			title : 'hereee..',
-			content : '首页',
-			users: users
+			content : 'data页面',
+			users : users
 		}));
 	});
 });
